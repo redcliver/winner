66e4d9bd98fe80ef783ccfd1dc0d562f6d047529
igor <igor-peres@hotmail.com>
2019-01-30 13:08:50 -0300

    1.1

--- home/views.py
+++ home/views.py
@@ -5,14 +5,15 @@ from django.contrib.auth.models import User
 def home(request):
     if request.user.is_authenticated:
         cargo = request.user.last_name
+        nome = cargo = request.user.get_short_name()
         if cargo == 'diretor':
-            return render(request, 'home/home.html', {'title':'Home'})
+            return render(request, 'home/home.html', {'title':'Home', 'nome':nome})
         elif cargo == 'recepcionista':
-            return render(request, 'home/home1.html', {'title':'Home'})
+            return render(request, 'home/home1.html', {'title':'Home', 'nome':nome})
         elif cargo == 'professor':
-            return render(request, 'home/home2.html', {'title':'Home'})
+            return render(request, 'home/home2.html', {'title':'Home', 'nome':nome})
         elif cargo == 'aluno':
-            return render(request, 'home/home3.html', {'title':'Home'})
+            return render(request, 'home/home3.html', {'title':'Home', 'nome':nome})
         return render(request, 'sistema_login/login.html', {'title':'Entrar'})
     else:
         return render(request, 'sistema_login/login.html', {'title':'Entrar'})
